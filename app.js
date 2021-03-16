37bb44a6141fe558d71c8e61d88ef1311df3664f
SakibHasanCse <sakibhasan9641@gmail.com>
2021-03-16 22:41:03 +0600

    show password

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express')
 const app = express()
 const wifiPassword = require('wifi-password');
 
+// Get current wifi password
+
 app.get('/', function(req, res) {
     wifiPassword().then(password => {
         console.log(password);
-        return res.json({ 'password': password })
+        return res.json({ 'success': `Your Wifi Password is : ${password}` })
             //=> 'johndoesecretpassword'
-    });
+    }).catch(err => {
+        return res.json({ 'error': 'Access denied , try agine with your username and password' })
+
+    })
 
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
---
+++ readme.md
@@ -0,0 +1,5 @@
+Get current wifi password
+
+Returns a promise that resolves to a string containing the password.
+
+Get the wifi password for a specified known network.
